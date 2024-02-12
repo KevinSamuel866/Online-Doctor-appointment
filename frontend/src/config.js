f848ed63c3ef33dc66d66bb34f82c1596ffcec25
KevinSamuel866 <kevinsamuel866@gmail.com>
2024-02-12 15:07:41 +0200

    change the link

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,2 @@
-export const BASE_URL = "https://https://oda-ze5d.onrender.com/api/v1";
-// export const BASE_URL = "https://medicare-booking-website.onrender.com/api/v1";
+export const BASE_URL = "https://oda-ze5d.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
